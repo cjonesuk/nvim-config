efd3ed7421a770b15972ed7030507385ab7ecfe2
Christopher Jones <christopher.jones2@yum.com>
2024-05-15 09:11:18 +0100

    telescope

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,15 +1,13 @@
-print 'telescope.lua'
 return {
-    { dir = "~/plugins/plenary.nvim" },
-    {
-      dir = "~/plugins/telescope.nvim/",
-      tag = "0.1.6",
-      dependencies = {
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-        { "nvim-telescope/telescope-smart-history.nvim" },
-      },
-      config = function()
-        require "custom.telescope"
-      end,
-    },
-  }
+  'nvim-telescope/telescope.nvim',
+  tag = "0.1.6",
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "nvim-telescope/telescope-smart-history.nvim" },
+  },
+  config = function()
+    require "custom.telescope"
+  end,
+}
+ 
