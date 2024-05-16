3136a87a9315615403826b2dc7b26003c5bd8429
Christopher Jones <christopher.jones2@yum.com>
2024-05-16 20:34:03 +0100

    options and nav keymaps

---
+++ plugin/keymaps.lua
@@ -0,0 +1,10 @@
+
+local set = vim.keymap.set
+
+print "Setting up keymaps"
+
+-- Navigate between splits
+set("n", "<c-j>", "<c-w><c-j>")
+set("n", "<c-k>", "<c-w><c-k>")
+set("n", "<c-l>", "<c-w><c-l>")
+set("n", "<c-h>", "<c-w><c-h>")
---
+++ plugin/options.lua
@@ -0,0 +1,17 @@
+local opt = vim.opt
+
+-- Line numbers
+opt.number = true
+opt.relativenumber = true
+
+opt.splitbelow = true
+opt.splitright = true
+
+-- Search
+opt.smartcase = true
+opt.ignorecase = true
+
+opt.signcolumn = "yes"
+
+-- Clipboard
+opt.clipboard = "unnamedplus"
