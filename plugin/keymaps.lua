81059c66d0a9b3cdd977a3041d3e6e319688e83d
Christopher Jones <christopher.jones2@yum.com>
2024-05-17 16:18:58 +0100

    resize splits

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,10 +1,13 @@
-
 local set = vim.keymap.set
-
-print "Setting up keymaps"
-
+ 
 -- Navigate between splits
 set("n", "<c-j>", "<c-w><c-j>")
 set("n", "<c-k>", "<c-w><c-k>")
 set("n", "<c-l>", "<c-w><c-l>")
 set("n", "<c-h>", "<c-w><c-h>")
+
+-- Resize splits
+set("n", "<M-,>", "<c-w>5>")
+set("n", "<M-.>", "<c-w>5<")
+set("n", "<M-t>", "<C-W>+")
+set("n", "<M-s>", "<C-W>-")
