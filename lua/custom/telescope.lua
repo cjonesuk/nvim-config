ed485c214173301691b3317e20fb0d0ab0b6bf6e
Christopher Jones <christopher.jones2@yum.com>
2024-05-18 18:10:20 +0100

    telescope keymaps

--- lua/custom/telescope.lua
+++ lua/custom/telescope.lua
@@ -15,6 +15,3 @@ require("telescope").setup {
 pcall(require("telescope").load_extension, "fzf")
 pcall(require("telescope").load_extension, "smart_history")
 
-local builtin = require "telescope.builtin"
-
-local set = vim.keymap.set
--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,5 +1,6 @@
 local set = vim.keymap.set
- 
+local telescope = require "telescope.builtin"
+
 -- Navigate between splits
 set("n", "<c-j>", "<c-w><c-j>")
 set("n", "<c-k>", "<c-w><c-k>")
@@ -11,3 +12,14 @@ set("n", "<M-,>", "<c-w>5>")
 set("n", "<M-.>", "<c-w>5<")
 set("n", "<M-t>", "<C-W>+")
 set("n", "<M-s>", "<C-W>-")
+
+-- Telescope
+set("n", "<space>sf", telescope.find_files, { desc = "[S]earch [F]iles" })
+set("n", "<space>sh", telescope.help_tags, { desc = "[S]earch [H]elp" })
+set("n", "<space>sg", telescope.live_grep, { desc = "[S]earch [G]rep" })
+set("n", "<space>s/", telescope.current_buffer_fuzzy_find, { desc = "[S]earch current buffer fuzzily" })
+
+-- Shortcut for searching your Neovim configuration files
+vim.keymap.set("n", "<space>sn", function()
+    telescope.find_files({ cwd = vim.fn.stdpath("config") })
+end, { desc = "[S]earch [N]eovim files" })
