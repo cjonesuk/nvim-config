0679ca6691caca4d5f381b0b7f4799f8e082edc8
Christopher Jones <christopher.jones2@yum.com>
2024-05-28 17:17:26 +0100

    exit insert mode with jj

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -11,6 +11,8 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float,
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist,
                {desc = 'Open diagnostic [Q]uickfix list'})
 
+vim.keymap.set('i', 'jj', '<Esc>', {desc = 'Exit insert mode with jj'})
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
